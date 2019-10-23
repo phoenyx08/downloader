fe7c8a1baf11857f8bd77fa7f445147bdb9350cd
Yuriy Serdyuk <yury@serdyuk.pp.ua>
2019-10-23 23:46:51 +0300

    Added some commentaries

--- src/Downloader/FakeDownloader.php
+++ src/Downloader/FakeDownloader.php
@@ -8,10 +8,24 @@
 
 namespace PhoenyxStudio\Downloader;
 
+/**
+ * Used for development purposes.
+ *
+ * This class accepts arbitrary string and always returns the same html-markup. It simulates behavior of a class
+ * which downloads some html from some location. The class might be needed when a developer does not wish to generate
+ * network traffic and just trying to test some code over result provided by the downloader
+ *
+ * @package PhoenyxStudio\Downloader
+ */
 class FakeDownloader implements IDownloader
 {
     /**
-     * @param string $url
+     * Downloads html-source of a webpage.
+     *
+     * Just returns the same html for any input argument in case of FakeDownloader
+     *
+     * @param string $url Url of the webpage for which we want to get html-source (in general case, in case of
+     * FakeDownloader can accept any string)
      * @return string
      */
     public function download(string $url): string
--- src/Downloader/IDownloader.php
+++ src/Downloader/IDownloader.php
@@ -2,7 +2,22 @@
 
 namespace PhoenyxStudio\Downloader;
 
+/**
+ * Interface IDownloader.
+ *
+ * Provides methods due to implement in downloader classes
+ *
+ * @package PhoenyxStudio\Downloader
+ */
 interface IDownloader
 {
+    /**
+     * Main function in a class implementing Downloader interface.
+     *
+     * Function accepting url and returning html-source of the page located at the url
+     *
+     * @param string $url Pass this Url to get some html
+     * @return string If it's all right source-code of the page at $url returned here
+     */
     public function download(string $url) : string;
 }
